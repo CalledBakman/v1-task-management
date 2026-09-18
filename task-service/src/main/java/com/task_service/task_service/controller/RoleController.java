3aecc08d0d8cfc50bfa005d5616b2925da78f0fc
CalledBakman <bakmandev@gmail.com>
2026-09-18 19:34:22 +0330

    new API (GET api/roles): it returns all types of roles that we have in the database

---
+++ task-service/src/main/java/com/task_service/task_service/controller/RoleController.java
@@ -0,0 +1,26 @@
+package com.task_service.task_service.controller;
+
+import com.task_service.task_service.dto.RoleDTO;
+import com.task_service.task_service.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/roles")
+public class RoleController {
+
+    @Autowired
+    private RoleService service;
+
+    @GetMapping
+    public ResponseEntity<List<RoleDTO>> getRoles(){
+        return new ResponseEntity<>(service.getRoles(), HttpStatus.OK);
+    }
+
+}
